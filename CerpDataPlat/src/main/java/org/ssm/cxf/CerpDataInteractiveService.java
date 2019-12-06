9841ad9b0e560b2991051f3cccb4eb62b97df2d4
tsl-lizhiguang <tsl-lizhiguang@tasly.com>
2019-12-06 16:50:17 +0800

    修改格式

--- CerpDataPlat/src/main/java/org/ssm/cxf/CerpDataInteractiveService.java
+++ CerpDataPlat/src/main/java/org/ssm/cxf/CerpDataInteractiveService.java
@@ -4,7 +4,9 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService(name="CerpDataInteractiveService",targetNamespace="http://cxf.ssm.org/")
-public interface CerpDataInteractiveService {	
+public interface CerpDataInteractiveService {
+	
 	public String tslCerpDataInteractive(@WebParam(name="userName")  String userName, @WebParam(name="passWord")  String passWord, @WebParam(name="oper")  String oper, @WebParam(name="xmlData")  String xmlData);
+	
 	public String tslCerpDataUpdate(@WebParam(name="userName")  String userName, @WebParam(name="passWord")  String passWord, @WebParam(name="oper")  String oper, @WebParam(name="xmlData")  String xmlData);
 }
