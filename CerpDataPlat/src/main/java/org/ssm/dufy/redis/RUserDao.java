69d1f933950f112be180e7cb5d8b0adf1cb02728
tsl-lizhiguang <tsl-lizhiguang@tasly.com>
2020-01-09 10:03:19 +0800

    增加redis.propertise文件

---
+++ CerpDataPlat/src/main/java/org/ssm/dufy/redis/RUserDao.java
@@ -0,0 +1,28 @@
+package org.ssm.dufy.redis;
+
+//import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+import org.ssm.dufy.entity.User;
+
+@Repository
+public class RUserDao {
+	
+	@Autowired
+	RedisTemplate<String, User> redisTemplate;
+	
+	public void setUser(User user) {
+		ValueOperations<String, User> ops = redisTemplate.opsForValue();
+//		ops.set(user.getUserName(), user, 360, TimeUnit.SECONDS);
+		ops.set(user.getUserName(), user);
+	}
+	
+	public User getUser(String username) {
+		ValueOperations<String, User> ops = redisTemplate.opsForValue();
+		return ops.get(username);
+	}
+
+}
